774112822168d39845c741e47674d7dc449a1bb9
Alban Andrieu <alban.andrieu@free.fr>
2020-04-11 22:47:58 +0200

    Add zfs and twitter

--- conky/startconky.sh
+++ conky/startconky.sh
@@ -9,7 +9,7 @@ conky -p 5 -c ~/.conky/conky_weather &
 conky -p 5 -c ~/.conky/conky_weather_oslo &
 conky -p 5 -c ~/.conky/conky_market 
 
-conky -p 5 -c ~/.conkyGreen\ Apple\ Desktop/Gotham
+conky -p 5 -c ~/.conky/Green\ Apple\ Desktop/Gotham
 conky -p 5 -c ~/.conky/ConkyOrange/conkyrc_orange_4k
 
 exit 0
