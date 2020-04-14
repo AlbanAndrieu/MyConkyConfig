9dcae9b077f39539ef90adf2cf141a30576f1c19
Alban Andrieu <alban.andrieu@free.fr>
2020-04-14 20:23:17 +0200

    Update

--- conky/conky-startup.sh
+++ conky/conky-startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -xv
 
-sleep 20s
-killall conky
+#sleep 20s
+killall -s SIGKILL conky
 #cd "{{ conky_home }}/{{ conky_theme }}"
 #conky -p 20 -c "~/{{ conky_theme }}/{{ conky_widget }}" &
-/home/albandrieu/.conky/startconky.sh
+$HOME/.conky/startconky.sh
 
 exit 0
--- conky/startconky.sh
+++ conky/startconky.sh
@@ -2,13 +2,17 @@
 sleep 30
 
 mkdir /home/albandrieu/.cache/oslo || true
+#ls -lrta /opt/conky/bin/conky
+#ls -lrta /usr/lib/x86_64-linux-gnu/libcairo.so
 #conky -p 5 -c ~/.conky/conky_earthquake &
 
 #conky -p 5 -c ~/.conky/conky_system &
 conky -p 5 -c ~/.conky/conky_weather &
 conky -p 5 -c ~/.conky/conky_weather_oslo &
 conky -p 5 -c ~/.conky/conky_market &
-conky -p 5 -c ~/.conky/Green\ Apple\ Desktop/Gotham &
+conky -p 5 -c ~/.conky/Gotham/Gotham &
 conky -p 5 -c ~/.conky/ConkyOrange/conkyrc_orange_4k
+
+#./convert.lua your_config.
 
 exit 0
